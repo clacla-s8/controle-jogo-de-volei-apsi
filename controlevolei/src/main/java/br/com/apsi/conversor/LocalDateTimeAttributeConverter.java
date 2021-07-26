6566ac057eea5fb6cdae43e4fecf0f6a5c0b5a11
Clarice Santos <clariceks8@gmail.com>
2021-07-26 10:34:20 -0300

    conversao data hora

---
+++ controlevolei/src/main/java/br/com/apsi/conversor/LocalDateTimeAttributeConverter.java
@@ -0,0 +1,21 @@
+package br.com.apsi.conversor;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import java.sql.Timestamp;
+
+@Converter(autoApply = true)
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+     
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime locDateTime) {
+        return locDateTime == null ? null : Timestamp.valueOf(locDateTime);
+    }
+ 
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp sqlTimestamp) {
+        return sqlTimestamp == null ? null : sqlTimestamp.toLocalDateTime();
+    }
+}
